c47bcc85d37bf98b3d356d021473e638ead12bff
¡Hola! soy Gabriela Chavez <denice.toledo00@gmail.com>
2025-08-19 17:48:50 +0000

    Autenticación JWT con Flask y React

--- src/api/models.py
+++ src/api/models.py
@@ -1,14 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from flask.bcrypt import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
+
+    def set_password(self, password: str):
+        """Genera el hash a partir de la contraseña en texto plano."""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        """Verifica si la contraseña ingresada coincide con el hash guardado."""
+        return check_password_hash(self.password_hash, password)
 
 
     def serialize(self):
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +13,66 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+# Nuevo endpoint: Registro
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    if not data:
+        return jsonify({"msg": "data no encontrada"}), 400
 
-    return jsonify(response_body), 200
+    campos_requeridos= ["email", "password"]
+    for campo in campos_requeridos: 
+        if campo not in data:
+            return jsonify({"msg": f"el campo '{campo}' es requerido"}), 400
+        
+
+    email_existente= User.query.filter_by(email=data["email"]).first()
+    if email_existente:
+        return jsonify({"msg": "Este email ya está registrado"}), 409    
+
+
+    try:
+
+        nuevo_usuario= User(
+            email=data["email"],
+            password=data["password"]
+        )
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+
+        return jsonify({"msg": "Usuario registrado con éxito", 
+                        "email": nuevo_usuario.serialize()}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+
+ #Login modificar 
+@api.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not user.check_password(password):
+        return jsonify({"error": "Credenciales inválidas"}), 401
+
+    # crea token de acceso
+    access_token = create_access_token(identity=user.id)
+    return jsonify({
+        "msg": "Login exitoso",
+        "token": access_token,
+        "user_id": user.id,
+        "email": user.email
+    }), 200
+
+
+# Nuevo endpoint: Private (requiere JWT válido)modificar
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def private():
+    user_id = get_jwt_identity()      # obtiene el user.id desde el token
+    user = User.query.get(user_id)
+    return jsonify({"ok": True, "user": user.serialize()}), 200
